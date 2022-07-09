2c2392eb13c770626ce421b2874ab7a41f65e742
Rahaf <rahaflh.900@hotmail.com>
2022-07-09 19:49:52 +0200

    Initial commit

---
+++ actividad/build/lib/misfunciones/rpy.py
@@ -0,0 +1,30 @@
+from rpy2.robjects import r
+
+r('''
+n=20
+  
+# arranging sequence
+x = seq(1, n)
+  
+# creating an empty place to store the numbers
+prime_numbers=c()
+  
+for (i in seq(2, n)) {
+  if (any(x == i)) {
+  
+    # prime numbers gets stored in a sequence order
+    prime_numbers = c(prime_numbers, i)
+    x = c(x[(x %% i) != 0], i)
+  }
+  
+}
+  
+# printing the series
+print(prime_numbers)
+  
+''')
+
+
+
+
+
---
+++ actividad/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(
+    name='misfunciones',
+    version='1.0',
+    packages=['.misfunciones']
+)
