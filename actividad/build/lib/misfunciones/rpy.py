0b1504ce7496bb2c64dfa56140fac5b2c3046abe
Rahaf <rahaflh.900@hotmail.com>
2022-07-09 21:10:42 +0200

    ¨edit¨

--- actividad/build/lib/misfunciones/rpy.py
+++ actividad/build/lib/misfunciones/rpy.py
@@ -1,28 +1,34 @@
-from rpy2.robjects import r
+import rpy2.robjects as ro
 
-r('''
-n=20
-  
-# arranging sequence
-x = seq(1, n)
-  
-# creating an empty place to store the numbers
-prime_numbers=c()
-  
-for (i in seq(2, n)) {
-  if (any(x == i)) {
-  
-    # prime numbers gets stored in a sequence order
-    prime_numbers = c(prime_numbers, i)
-    x = c(x[(x %% i) != 0], i)
+codigo_r = ('''
+prime <- function(n){
+    
+  # arranging sequence
+  x = seq(1, n)
+    
+  # creating an empty place to store the numbers
+  prime_numbers=c()
+    
+  for (i in seq(2, n)) {
+    if (any(x == i)) {
+    
+      # prime numbers gets stored in a sequence order
+      prime_numbers = c(prime_numbers, i)
+      x = c(x[(x %% i) != 0], i)
+    }
+
   }
-  
-}
-  
-# printing the series
-print(prime_numbers)
-  
+    
+  # printing the series
+  print(prime_numbers)
+} 
 ''')
+
+number = int(input("Introduce un número: "))
+
+ro.r(codigo_r)
+prime_py = ro.globalenv['prime']
+prime_py(number)
 
 
 
